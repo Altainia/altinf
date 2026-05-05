484e56bef3423ce2d142068023144caa631e15b7
altainia <altainia@gmail.com>
2026-05-04 19:22:12 -0500

    feat: add session_token Dbo model

---
+++ src/auth/session_token.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Wt/Dbo/Dbo.h>
+
+#include <string>
+
+struct session_token
+{
+	std::string token_hash;
+	std::string username;
+
+	template<class Action>
+	void persist(Action& a)
+	{
+		Wt::Dbo::field(a, token_hash, "token_hash");
+		Wt::Dbo::field(a, username, "username");
+	}
+};
